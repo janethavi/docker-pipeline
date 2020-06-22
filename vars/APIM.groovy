c31dc0253fc740c2f643ceaff8b153315e1bee6a
Janeth <janethavishka@gmail.com>
2020-06-22 14:50:11 +0530

    Change directory structure

--- vars/APIM.groovy
+++ vars/APIM.groovy
@@ -19,35 +19,7 @@ def call() {
                     withCredentials([usernamePassword(credentialsId: 'docker-image-build', passwordVariable: 'WUM_PASSWORD', usernameVariable: 'WUM_USERNAME')]) {
                         sh 'wum init -u $WUM_USERNAME -p $WUM_PASSWORD'
                         //sh '${WORKSPACE}/scripts/wum-update.sh $wso2_product $wso2_product_version'
-                        sh '''#!/bin/bash
-
-                            readonly wso2_product_name=$wso2_product
-                            readonly wso2_product_version=$wso2_product_version
-                            readonly wso2_product_host_location="${WORKSPACE}/product-packs"
-                            readonly WUM_HOME="${HOME}/.wum3"
-                            product_pack_name=""
-                            echo "Adding ${wso2_product_name}-${wso2_product_version}"
-                            wum add ${wso2_product_name}-${wso2_product_version} -y &
-                            local pid=$!
-                            wait $pid
-                            echo "Updating ${wso2_product_name}-${wso2_product_version}"
-                            wum update ${wso2_product_name}-${wso2_product_version} full &
-                            pid=$!
-                            wait $pid
-                            product_pack_name=$(wum list 2> /dev/null | awk '{print $3}' | grep -e "${wso2_product_name}.*full\.zip")
-                            make_directory=$(which mkdir)
-                            wso2_product="${wso2_product_name}-${wso2_product_version}"
-                            wum_product_home="${WUM_HOME}/products/${wso2_product_name}/${wso2_product_version}"
-                            product_pack_path="${wum_product_home}/full/${product_pack_name}"
-                            echo "Moving ${wso2_product} to $wso2_product_host_location"
-                            [[ ${make_directory} ]] && test ! -d ${wso2_product_host_location} && ${make_directory} ${wso2_product_host_location}
-                            test -f ${product_pack_path} && test -d ${wso2_product_host_location} && mv ${product_pack_path} ${wso2_product_host_location}
-                            echo "Hosting product pack in localhost:8888"
-                            pushd ${wso2_product_host_location};
-                            python -m SimpleHTTPServer 8888 &
-                            sleep 5
-                            popd
-                        '''
+                        
                     }
                 }
             }
