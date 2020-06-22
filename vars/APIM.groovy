b86e258b91d7652e0fa235f365aced753bf5676b
Janeth <janethavishka@gmail.com>
2020-06-22 15:37:58 +0530

    add chmod

--- vars/APIM.groovy
+++ vars/APIM.groovy
@@ -24,6 +24,7 @@ def call() {
                         wum_update_script = libraryResource 'org/wso2/ie/scripts/wum-update.sh'
                         writeFile file: './wum-update.sh', text: wum_update_script
                         sh 'ls'
+                        sh 'chmod +x ${WORKSPACE}/wum-update.sh'
                         sh '${WORKSPACE}/wum-update.sh $wso2_product $wso2_product_version'
                     }
                 }
