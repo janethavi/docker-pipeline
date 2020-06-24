d9a419967da9c0de4915379d7fec274d4774c250
Janeth <janethavishka@gmail.com>
2020-06-24 17:18:56 +0530

    Clean code

--- resources/org/wso2/ie/scripts/wum-update.sh
+++ resources/org/wso2/ie/scripts/wum-update.sh
@@ -55,53 +55,14 @@ function move_pack_to_destination() {
     local pack_name_with_timestamp=$(ls ${wum_product_home}/full |  ${GREP} -e "${product_pack_name}.*full\.zip" )
     local product_pack_path="${wum_product_home}/full/${pack_name_with_timestamp}"
 
-    # echo "----------"
-    # ls ${wum_product_home}
-    # ls ${product_pack_path}
-    # echo "----------"
-
-    # # clean the existing product pack folder
-    # echo "Cleaning Hosting pack directory"
-    # ${TEST} -d ${wso2_product_host_location} && ${REMOVE} -r ${wso2_product_host_location}/*
-
     echo "Moving ${wso2_product} to $wso2_product_host_location"
     [[ ${make_directory} ]] && ${TEST} ! -d ${wso2_product_host_location} && ${make_directory} ${wso2_product_host_location}
     ${TEST} -f ${product_pack_path} && ${TEST} -d ${wso2_product_host_location} && ${MOVE} ${product_pack_path} ${wso2_product_host_location}
 }
 
-# function get_product_packs() {
-#     # obtain the currently available WSO2 product packs for a particular product based on the WUM channel
-#     product_pack_name="${wso2_product_name}-${wso2_product_version}"
-#     product_packs=$(${WUM} list 2> /dev/null | ${AWK} '{print $3}' | ${GREP} -e "${product_pack_name}.*full\.zip")
-
-#     wso2_product_packs=()
-#     for product_pack in ${product_packs}; do
-#         wso2_product_packs+=("${product_pack}")
-#     done
-# }
-
-# function clean_up() {
-#     # remove unnecessary product packs
-#     local removable_packs=("${wso2_product_packs[@]:0:${#wso2_product_packs[@]}-1}")
-#     echo ${wso2_product_packs[@]}
-#     for pack in ${removable_packs[@]}
-#     do
-#     if ! ${WUM} delete -f ${pack}; then
-#         echo "Failed to remove the WUM based product pack ${pack}. Exiting !"
-#     else
-#         echo "WUM based product pack ${pack} is deleted !"
-#     fi
-#     done
-# }
-
 function write_config_file(){
     echo "Writing timestamp to properties file"
-    # echo "loc"
-    # ls ${wso2_product_host_location}
-    # echo "grep"
-    # ls ${wso2_product_host_location} | grep -e '${wso2_product_name}-${wso2_product_version}.*full.zip'
     local timestamp=$(ls ${wso2_product_host_location} | ${GREP} -e "${wso2_product_name}-${wso2_product_version}.*full.zip" | ${CUT} -d'+' -f2 | ${CUT} -d'.' -f1)
-    # echo $timestamp
     local string_value="wum_timestamp=${timestamp}"
     echo ${string_value} > $properties_file_name
 }
@@ -112,15 +73,9 @@ function host_products(){
     python3 -m http.server 8888 &
     sleep 5
     popd
-    # curl http://localhost:8888/
 }
 
 download_apim_product
-#get_product_packs
 move_pack_to_destination
-#clean_up
 write_config_file
 host_products
-
-# echo "List product-pack Dir..."
-# ls ${wso2_product_host_location}
