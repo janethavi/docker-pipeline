b601e6fc35449c8cbd20e68539730a1d11981e80
Janeth <janethavishka@gmail.com>
2020-06-25 11:24:36 +0530

    Edit groovy class

--- src/org/wso2/ie/utils/APIMUtils.groovy
+++ src/org/wso2/ie/utils/APIMUtils.groovy
@@ -53,11 +53,11 @@ def get_product_docker_home(wso2_product) {
 
 def get_docker_release_version(wso2_product, wso2_product_version) {
     println "Getting Docker Release Version..."
-    wum_update_script = libraryResource 'org/wso2/ie/conf/apim-data.json'
+    def wum_update_script = libraryResource 'org/wso2/ie/conf/apim-data.json'
     writeFile file: './apim-data.json', text: wum_update_script
     config_file = readJSON file: 'apim-data.json'
     println("---------")
-    println(APIMUtils.product_key)
+    println(this.product_key)
     def result = config_file.profiles.find{ it.product == wso2_product }?.versions?.find{ it.product_version == wso2_product_version }
     DOCKER_RESOURCES_GIT_RELEASE_TAG = result.docker_release_version
     latest_version = result.latest
