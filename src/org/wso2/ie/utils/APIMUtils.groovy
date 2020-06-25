8d38279e5538138765dbc7c38caa0c7c2a314729
Janeth <janethavishka@gmail.com>
2020-06-25 13:42:02 +0530

    write file

--- src/org/wso2/ie/utils/APIMUtils.groovy
+++ src/org/wso2/ie/utils/APIMUtils.groovy
@@ -59,6 +59,8 @@ def get_docker_release_version(wso2_product, wso2_product_version) {
     // writeFile file: './apim-data.json', text: wum_update_script
     // config_file = readJSON file: 'apim-data.json'
     // println("---------")
+    String writeFile = this.getClass().getResource('org/wso2/ie/conf/apim-data.json').text
+    writeFile file: './apim-data.json', text: wum_update_script
     println(this.product_key)
     def result = config_file.profiles.find{ it.product == wso2_product }?.versions?.find{ it.product_version == wso2_product_version }
     DOCKER_RESOURCES_GIT_RELEASE_TAG = result.docker_release_version
