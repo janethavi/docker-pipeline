344eb707d58e2494b6b2d36abaf70ef03e5a9b0f
Janeth <janethavishka@gmail.com>
2020-06-25 14:38:30 +0530

    Add data type

--- src/org/wso2/ie/utils/APIMUtils.groovy
+++ src/org/wso2/ie/utils/APIMUtils.groovy
@@ -24,6 +24,7 @@ def latest_version
 
 def get_product_docker_home(wso2_product) {
     println "Getting product Docker Homes..."
+    def product_profile_docker_homes
     switch(wso2_product) {
         case "wso2am":
             product_profile_docker_homes = ["apim"]
