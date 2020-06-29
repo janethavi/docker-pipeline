c83741687785d01ad568d960bd2f8d408c24c987
Janeth <janethavishka@gmail.com>
2020-06-29 21:47:47 +0530

    Add global variable

--- vars/APIM.groovy
+++ vars/APIM.groovy
@@ -38,7 +38,7 @@ def call(product_key) {
                         sh 'wum init -u $WUM_USERNAME -p $WUM_PASSWORD'
                     }
                     script{
-                        wum_update_script = libraryResource '${SCRIPT_FILE_LOCATION}/wum-update.sh'
+                        wum_update_script = libraryResource "${SCRIPT_FILE_LOCATION}/wum-update.sh"
                         writeFile file: './wum-update.sh', text: wum_update_script
                         sh 'chmod +x ${WORKSPACE}/wum-update.sh'
                         sh '${WORKSPACE}/wum-update.sh $wso2_product $wso2_product_version'
