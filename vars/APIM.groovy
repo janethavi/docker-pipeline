23da4c7d1e80a4a439e2432d4cf59d45089a3751
Janeth <janethavishka@gmail.com>
2020-06-30 15:46:52 +0530

    Add build status on email subject

--- vars/APIM.groovy
+++ vars/APIM.groovy
@@ -65,7 +65,7 @@ def call(product_key) {
         }
         post {
             always {
-                emailext body: 'Check console output at $BUILD_URL to view the results. \n\n -------------------------------------------------- \n${BUILD_LOG, maxLines=100, escapeHtml=false}', 
+                emailext body: "Check console output at ${BUILD_URL} to view the results. \n\n -------------------------------------------------- \n ------- Build Info ------- \n Product:- ${wso2_product} Version:- ${wso2_product_version} Build Status:- ${env.BUILD_STATUS}", 
                 to: "${EMAIL_TO}",
                 subject: "Build ${env.BUILD_STATUS} in Docker Image Build Jenkins: ${env.JOB_NAME} - #${env.BUILD_NUMBER}"
                 script{
